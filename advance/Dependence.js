d675d11f7acfafd73465abec41525d35851f5b56
lqavtar <15223488700@163.com>
2018-12-12 21:58:40 -0800

    ddd

--- advance/Dependence.js
+++ advance/Dependence.js
@@ -13,19 +13,30 @@ let abc = async function (moduleArray,later,former=1) {
     moduleArray.forEach(element => {
         add.push(`'`+element.id+`'`)
     })  
-    let para=`(`+add.join(',')+`)`
+    let para=add.join(',')
 
     console.log('######3',para)
   
-    let souInsert1=`delete acticle..tableTmp    
+    let souInsert1=
+    `
+    delete acticle..tableTmp    
        
     insert into acticle..tableTmp  
-     select * from acticle..tableResult  where  souModuleID in ${para} and tarModuleID   in ${para}
+     select * from acticle..tableResult 
+     where  1=1
+and tarModuleID in (${para},'Module00236')   and souModuleID  in (${para},'Module00236')  
+
+and not (tarModuleID  in ('Module00236') and  souModuleID  in ('Module00236') )
     
     delete acticle..tableTmp3
 
     insert into acticle..tableTmp3
-    select souFormID from acticle..tableResult  where  souModuleID in ${para} and tarModuleID   in ${para} `
+    select souFormID from acticle..tableResult  
+    where  1=1
+    and tarModuleID in (${para},'Module00236')   and souModuleID  in (${para},'Module00236')  
+    
+    and not (tarModuleID  in ('Module00236') and  souModuleID  in ('Module00236') )
+    `
 
     let souInsert2=    // 输出
     `insert into acticle..tableTmp
@@ -44,14 +55,72 @@ let abc = async function (moduleArray,later,former=1) {
 
     
     async function a(){
+        console.log('souInsert1:',souInsert1)
         await queryFunc(souInsert1)      
 
         for(let i=1;i<=later;i++){
             await queryFunc(souInsert2)
+            console.log('souInsert2:',souInsert2)
         }    
     }
     a()
-    let re= await queryFunc(`select * from acticle..tableTmp`) 
+    let qStr=`;with my as(
+        SELECT distinct
+         [tarFormName]
+              ,[souFormName]
+              ,[tarIsBusiness]
+              ,[souIsBusiness]
+              ,[tarIsDisabled]
+              ,[souIsDisabled]
+              ,[tarFormID]
+              ,[souFormID]
+              ,[tarModuleName]
+              ,[tarModuleID]
+              ,[souModuleName]
+              ,[souModuleID]
+              ,[tag]
+          FROM [acticle].[dbo].[tableTmp]
+          ),
+          you as(
+          select 
+           [tarFormName]
+              ,[souFormName]
+              ,[tarIsBusiness]
+              ,[souIsBusiness]
+              ,[tarIsDisabled]
+              ,[souIsDisabled]
+              ,[tarFormID]
+              ,[souFormID]
+              ,[tarModuleName]
+              ,[tarModuleID]
+              ,[souModuleName]
+              ,[souModuleID]
+              ,case when SUM(convert(int,[tag]))=2 then 'D'
+                    when SUM(convert(int,[tag]))=4 then 'U'
+                    when SUM(convert(int,[tag]))=8 then 'S'
+                    when SUM(convert(int,[tag]))=10 then 'SD'
+                    when SUM(convert(int,[tag]))=12 then 'SU'
+                    when SUM(convert(int,[tag]))=14 then 'SUD'
+                    when SUM(convert(int,[tag]))=6 then 'UD'
+                    else ''
+                end tag
+          from my
+          group by  [tarFormName]
+              ,[souFormName]
+              ,[tarIsBusiness]
+              ,[souIsBusiness]
+              ,[tarIsDisabled]
+              ,[souIsDisabled]
+              ,[tarFormID]
+              ,[souFormID]
+              ,[tarModuleName]
+              ,[tarModuleID]
+              ,[souModuleName]
+              ,[souModuleID]
+          )
+          select * from you 
+    `
+    let re= await queryFunc(qStr) 
     console.log('run end!')
 
     return re
--- routes/advance.js
+++ routes/advance.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');  //调用模板
 //創建編碼解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const abc=require('../advance/Dependence.js')
+const abc = require('../advance/Dependence.js')
 
 
 
@@ -52,13 +52,13 @@ router.post('/saveData', function (req, res) {
 
   /* req.body:{dt:,ModuleID:'001'} */
   let para = req.body.dt;
-  let ModuleID=req.body.ModuleID
+  let ModuleID = req.body.ModuleID
   //console.dir(req.body);
   //console.log('vegegefdfdfdfd');
   let writeData = (jjson) => {
     let fs = require('fs');
     let data = jjson;
-   //console.dir(jjson);
+    //console.dir(jjson);
 
     let baseDir = __dirname + '/../public/jsonFile/';
     let opts = {
@@ -66,85 +66,87 @@ router.post('/saveData', function (req, res) {
       encoding: 'utf8',
       stdio: [process.stdin, process.stdout, process.stderr]
     }
-    let fileName = baseDir +ModuleID+'.json';
+    let fileName = baseDir + ModuleID + '.json';
     console.dir(fileName);
     fs.writeFileSync(fileName, ` ${JSON.stringify(data, null, 2)}`, opts);
   }
 
   writeData(para);
-  let b=[];
+  let b = [];
   res.json(b);
 })
 
 //返回图
 router.post('/process_get', function (req, res) {
- 
-  console.log('**************************************************')
-  
 
-    //req.bod的形式
+  console.log('**************************************************')
+
+
+  //req.bod的形式
   /*   [ { id: 'Module00001', text: '人事基础资料' },
         { id: 'Module00002', text: '工程基础资料' },
         { id: 'Module00003', text: '品质基础资料' } ] */
 
   //如果没有在本地文件中找到对应模块的图，则执行的no函数,从数据库中查询
-  let no=async function () {
+  let no = async function () {
     // 输出 JSON 格式   
- 
-    let xxx=req.body
-    console.log('this is xxx:',xxx)
-   
 
-    let comps = await abc([xxx.body.shift()],xxx.slfMdID[0],xxx.slfMdID[1])  
+    let xxx = req.body
+    console.log('this is xxx:', xxx)
+    let a1 = [xxx.body.shift()]
+    console.log('send:', [a1, xxx.slfMdID[0], xxx.slfMdID[1]])
+    let comps = await abc(a1, xxx.slfMdID[0], xxx.slfMdID[1])
+
+
     let returnJson = new Array(); //最后的返回数据对象   
-
+    console.log('comps:', comps)
     for (let row of comps) {
       let tempObject = Object();//注意首字母大写
-     /*  tempObject.target = comps[key].targetFormName;                //目标表单名
-      tempObject.source = comps[key].sourceFormName;                //源表单名
-      tempObject.type = "resolved";
-      tempObject.rela = "主营产品";
-      tempObject.belongModule = comps[key].sourceModuleName;        //源模块名
-      tempObject.belongModuleID = comps[key].sourceModuleID;        //源模块ID */
+      /*  tempObject.target = comps[key].targetFormName;                //目标表单名
+       tempObject.source = comps[key].sourceFormName;                //源表单名
+       tempObject.type = "resolved";
+       tempObject.rela = "主营产品";
+       tempObject.belongModule = comps[key].sourceModuleName;        //源模块名
+       tempObject.belongModuleID = comps[key].sourceModuleID;        //源模块ID */
 
 
-      tempObject.target=row.tarFormName
-      tempObject.source=row.souFormName
+      tempObject.target = row.tarFormName
+      tempObject.source = row.souFormName
 
-      tempObject.tarIsBusiness=row.tarIsBusiness
-      tempObject.souIsBusiness=row.souIsBusiness
+      tempObject.tarIsBusiness = row.tarIsBusiness
+      tempObject.souIsBusiness = row.souIsBusiness
 
-      tempObject.tarIsDisabled=row.tarIsDisabled
-      tempObject.souIsDisabled=row.souIsDisabled
+      tempObject.tarIsDisabled = row.tarIsDisabled
+      tempObject.souIsDisabled = row.souIsDisabled
 
-      tempObject.tarFormID=row.tarFormID 
-      tempObject.souFormID=row.souFormID
+      tempObject.tarFormID = row.tarFormID
+      tempObject.souFormID = row.souFormID
 
-      tempObject.tarModuleName=row.tarModuleName
-      tempObject.tarModuleID=row.tarModuleID
-      
-      tempObject.souModuleName=row.souModuleName
-      tempObject.souModuleID=row.souModuleID
-      empObject.tag=row.tag
+      tempObject.tarModuleName = row.tarModuleName
+      tempObject.tarModuleID = row.tarModuleID
 
-      tempObject.rela=row.tarModuleID==row.souModuleID?'模块内':'模块间'
+      tempObject.souModuleName = row.souModuleName
+      tempObject.souModuleID = row.souModuleID
+      tempObject.tag = row.tag
+
+      tempObject.rela = row.tarModuleID == row.souModuleID ? '模块内' : '模块间'
 
 
       returnJson.push(tempObject);
     }
-    //console.dir(returnJson)
-    return returnJson   
-  }  
 
-  let ccc=async function(){
+    return returnJson
+  }
 
-    let myData=new Object();
+  let ccc = async function () {
+    let myData = new Object();
 
-    myData.body=await no();      
-    myData.isExist=0;
+    myData.body = await no();
+    myData.isExist = 0;
 
-    res.json(myData);       
-  } 
+    res.json(myData);
+    console.log('myData', myData)
+  }
   ccc()
 
 })
